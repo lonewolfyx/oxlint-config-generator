7821dafcab00721c84de930737f1005eaca70f15
lonewolfyx <877085893@qq.com>
2025-07-22 23:20:01 +0800

    fix: /api/payload.json

--- server/api/payload.json.ts
+++ server/api/payload.json.ts
@@ -2,37 +2,37 @@ import payload from '../payload.json'
 
 export default defineEventHandler(async () => {
     return {
-        // rules: payload,
-        // categories: [
-        //     {
-        //         name: 'correctness',
-        //         description: 'Rules to lint code that is definitely wrong or useless',
-        //     },
-        //     {
-        //         name: 'suspicious',
-        //         description: 'Rules to lint code that is likely to be wrong or useless',
-        //     },
-        //     {
-        //         name: 'pedantic',
-        //         description: 'Rules which are extra strict or might have false positives',
-        //     },
-        //     {
-        //         name: 'perf',
-        //         description: 'Rules that aim to improve the performance of code',
-        //     },
-        //     {
-        //         name: 'style',
-        //         description: 'Rules that help maintain a consistent style or enforce idiomatic syntax',
-        //     },
-        //     {
-        //         name: 'restriction',
-        //         description: 'Rules that ban specific patterns, syntax, or features and should be enabled on a case-by-case basis',
-        //     },
-        //     {
-        //         name: 'nursery',
-        //         description: 'Rules that are in development, may change significantly, or contain false positives',
-        //     },
-        // ],
+        rules: payload,
+        categories: [
+            {
+                name: 'correctness',
+                description: 'Rules to lint code that is definitely wrong or useless',
+            },
+            {
+                name: 'suspicious',
+                description: 'Rules to lint code that is likely to be wrong or useless',
+            },
+            {
+                name: 'pedantic',
+                description: 'Rules which are extra strict or might have false positives',
+            },
+            {
+                name: 'perf',
+                description: 'Rules that aim to improve the performance of code',
+            },
+            {
+                name: 'style',
+                description: 'Rules that help maintain a consistent style or enforce idiomatic syntax',
+            },
+            {
+                name: 'restriction',
+                description: 'Rules that ban specific patterns, syntax, or features and should be enabled on a case-by-case basis',
+            },
+            {
+                name: 'nursery',
+                description: 'Rules that are in development, may change significantly, or contain false positives',
+            },
+        ],
         plugins: [
             { name: 'typescript', isDefault: true },
             { name: 'unicorn', isDefault: true },
