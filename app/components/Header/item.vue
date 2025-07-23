87702e370b742ba9905cc56eb8d57b6147c8e6da
lonewolfyx <877085893@qq.com>
2025-07-23 16:17:27 +0800

    fix: numerical statistics

--- app/components/Header/item.vue
+++ app/components/Header/item.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="flex justify-start items-end text-xl font-sans gap-3">
         <span class="text-base opacity-50">Total number of rules:</span>
-        <span class="font-bold font-mono text-red-500">525</span>
+        <span class="font-bold font-mono text-red-500">{{ rulesTotal }}</span>
         <span class="text-base opacity-50">Rules turned on by default:</span>
-        <span class="font-bold font-mono text-green-500 opacity-50">123</span>
+        <span class="font-bold font-mono text-green-500 opacity-50">{{ defaultRules }}</span>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { payload } from '~/composables/payload'
+
 defineOptions({
     name: 'HeaderItem',
 })
+
+const rulesTotal = computed(() => payload.value.rules.length)
+
+const defaultRules = computed(() => payload.value.rules.filter(rule => rule.default).length)
 </script>
 
 <style scoped>
