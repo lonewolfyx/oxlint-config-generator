e96fe4f6f9184f7a26deae9633fd17b806d0cd0b
lonewolfyx <877085893@qq.com>
2026-02-17 21:06:08 +0800

    refactore(project): remove entire Nuxt.js project structure

--- app/plugins/pinia-persistedstate.ts
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    // @ts-ignore
-    nuxtApp.$pinia.use(piniaPluginPersistedstate)
-})
