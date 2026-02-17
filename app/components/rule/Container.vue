bbdaf0a5b7bd3cd0d052b3bf48ac8d9f2e33b6a3
lonewolfyx <877085893@qq.com>
2026-02-18 01:21:36 +0800

    feat(rule): implement rule selection interface with category filters

--- app/components/rule/Container.vue
+++ app/components/rule/Container.vue
@@ -1,12 +1,138 @@
 <template>
     <div
         :class="cn(
-            'bg-background grow',
-            'border border-input rounded-xl shadow-xs',
-            'flex flex-row min-h-0',
+            'flex flex-col grow',
+            'gap-4 min-h-0',
         )"
     >
-        <span>content</span>
+        <div class="flex flex-col gap-2">
+            <div class="flex items-start gap-2">
+                <span class="capitalize text-sm opacity-50">default:</span>
+                <div class="flex items-center gap-2">
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        eslint
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        oxc
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        typescript
+                    </Button>
+                </div>
+            </div>
+            <div class="flex items-start gap-2">
+                <span class="capitalize text-sm opacity-50">frameworks:</span>
+                <div class="flex items-center gap-2">
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        react
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        React Perf
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        JSX Accessibility
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        vue
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        next.js
+                    </Button>
+                </div>
+            </div>
+            <div class="flex items-start gap-2">
+                <span class="capitalize text-sm opacity-50">testing:</span>
+                <div class="flex items-center gap-2">
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        jest
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        vitest
+                    </Button>
+                </div>
+            </div>
+            <div class="flex items-start gap-2">
+                <span class="capitalize text-sm opacity-50">misc:</span>
+                <div class="flex items-center gap-2">
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        unicon
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        import
+                    </Button>
+                    <Button
+                        as="label"
+                        class="capitalize border rounded-full px-2.5 py-0.5 h-auto"
+                        variant="secondary"
+                    >
+                        jsdoc
+                    </Button>
+                </div>
+            </div>
+        </div>
+        <div class="flex items-center">
+            <div class="bg-muted border border-input rounded-2xl shadow-xs px-3 py-1">
+                762 rules enabled out of 1718 rules
+            </div>
+        </div>
+        <div
+            :class="cn(
+                'flex',
+                'px-4 py-2 bg-background border border-input rounded-xl shadow-xs min-h-0 h-full',
+            )"
+        >
+            <div class="grow overflow-y-auto min-h-0">
+                <div>rule selected container</div>
+            </div>
+        </div>
     </div>
 </template>
 
