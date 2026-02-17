acc833eff3cf4468c6a7d42518403af8d0e98aff
lonewolfyx <877085893@qq.com>
2026-02-18 01:24:11 +0800

    style(SiteRuleInfo): update component styling with padding and border

--- app/components/SiteRuleInfo.vue
+++ app/components/SiteRuleInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-start items-end text-xl font-sans gap-3">
+    <div class="flex justify-start items-end text-xl font-sans gap-3 pb-2 border-b border-dashed">
         <span class="text-base opacity-50">Total number of rules:</span>
         <span class="font-bold font-mono text-red-500">677</span>
         <span class="text-base opacity-50">Rules turned on by default:</span>
