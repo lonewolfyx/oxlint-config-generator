4693921c190174e8875ce8ca28283134381c4ba7
lonewolfyx <877085893@qq.com>
2026-02-18 21:32:54 +0800

    feat(rule): implement rule configuration UI with severity controls

---
+++ app/components/rule/Severity.vue
@@ -0,0 +1,28 @@
+<template>
+    <RadioGroup
+        :class="cn(
+            'bg-muted inline-flex items-center rounded',
+            'divide-x divide-border-input',
+            'p-1 gap-0',
+        )"
+        default-value="comfortable"
+    >
+        <RadioButtonGroupItem value="off">
+            off
+        </RadioButtonGroupItem>
+        <RadioButtonGroupItem value="warn">
+            warn
+        </RadioButtonGroupItem>
+        <RadioButtonGroupItem value="error">
+            error
+        </RadioButtonGroupItem>
+    </RadioGroup>
+</template>
+
+<script lang="ts" setup>
+import { cn } from '~/lib/utils'
+
+defineOptions({
+    name: 'RuleSeverity',
+})
+</script>
