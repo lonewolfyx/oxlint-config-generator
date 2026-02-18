7f78e27fcc30411f63ea685b0da8d716123039f7
lonewolfyx <877085893@qq.com>
2026-02-18 22:05:11 +0800

    refactor(rule): extract rule item component from container

--- app/components/rule/Container.vue
+++ app/components/rule/Container.vue
@@ -20,64 +20,7 @@
             <div class="grow overflow-y-auto min-h-0">
                 <div>rule selected container</div>
                 <div class="grid grid-cols-3 gap-4">
-                    <div class="relative border border-input rounded p-2 space-y-2">
-                        <div class="flex flex-col">
-                            <div class="flex justify-start items-center">
-                                <div class="flex items-center flex-1 gap-1.5">
-                                    <span class="font-mono">typescript</span>
-                                    <span class="font-medium opacity-65">/</span>
-                                </div>
-                                <Button
-                                    size="icon-sm"
-                                    variant="ghost"
-                                >
-                                    <Icon
-                                        mode="svg"
-                                        name="lucide:delete"
-                                    />
-                                </Button>
-                            </div>
-                            <span class="text-foreground font-medium">adjacent-overload-signatures</span>
-                        </div>
-                        <div class="text-xs opacity-75 min-h-12">
-                            Require that function overload signatures be consecutive.
-                        </div>
-                        <div class="flex justify-between items-center">
-                            <div class="flex items-center gap-2 flex-1">
-                                <Icon
-                                    class="text-green-500/50"
-                                    mode="svg"
-                                    name="si:check-square-duotone"
-                                    size="16"
-                                />
-                                <Icon
-                                    class="text-zinc-500/50"
-                                    mode="svg"
-                                    name="si:wrench-duotone"
-                                    size="16"
-                                />
-                                <Icon
-                                    class="opacity-50"
-                                    mode="svg"
-                                    name="noto:construction"
-                                    size="16"
-                                />
-                            </div>
-                            <div class="flex items-center gap-1">
-                                <RuleSeverity />
-                                <Button
-                                    size="icon-sm"
-                                    variant="secondary"
-                                    class="rounded"
-                                >
-                                    <Icon
-                                        mode="svg"
-                                        name="ph:sliders-duotone"
-                                    />
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
+                    <RuleItem />
                 </div>
             </div>
         </div>
