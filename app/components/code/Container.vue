e5ea6dbf17d4ddee40b671a9b36437726d1fbcca
lonewolfyx <877085893@qq.com>
2026-02-19 00:47:38 +0800

    feat(code): add toolbar buttons to code container

--- app/components/code/Container.vue
+++ app/components/code/Container.vue
@@ -7,14 +7,34 @@
         )"
     >
         <div class="grow flex flex-col min-h-0 pt-4 gap-4">
-            <div
-                :class="cn(
-                    'relative capitalize text-foreground font-mono font-medium',
-                    'mx-3 pl-4',
-                    'after:absolute after:inset-0 after:left-0 after:w-1 after:h-full after:rounded after:bg-amber-600',
-                )"
-            >
-                .oxlintrc.json
+            <div class="flex justify-between items-center gap-2 mx-3">
+                <div
+                    :class="cn(
+                        'relative capitalize text-foreground font-mono font-medium',
+                        'mr-auto pl-4',
+                        'after:absolute after:inset-0 after:left-0 after:w-1 after:h-full after:rounded after:bg-amber-600',
+                    )"
+                >
+                    .oxlintrc.json
+                </div>
+                <Button
+                    size="icon-sm"
+                    variant="secondary"
+                >
+                    <Icon
+                        mode="svg"
+                        name="lucide:refresh-ccw-dot"
+                    />
+                </Button>
+                <Button
+                    size="icon-sm"
+                    variant="secondary"
+                >
+                    <Icon
+                        mode="svg"
+                        name="lucide:copy"
+                    />
+                </Button>
             </div>
             <div class="rounded-b-xl p-2 overflow-y-auto min-h-0 h-full bg-muted">
                 <span>code review</span>
