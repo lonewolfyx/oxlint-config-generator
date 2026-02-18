47c87c7042d604ee085fbc92df5dce5a9b1d3071
lonewolfyx <877085893@qq.com>
2026-02-18 23:32:24 +0800

    feat(rule): update rule container with icon and enhanced styling

--- app/components/rule/Container.vue
+++ app/components/rule/Container.vue
@@ -7,16 +7,29 @@
     >
         <ScopeSelect />
         <div class="flex justify-between items-center">
-            <div class="bg-gray-200 dark:bg-gray-800/20 border border-input rounded-2xl px-3 py-1">
-                762 rules enabled out of 1718 rules
+            <div
+                :class="cn(
+                    'flex items-center gap-2',
+                    'bg-gray-200 dark:bg-gray-800/20 text-foreground text-sm',
+                    'border border-input rounded-2xl',
+                    'px-3 py-1',
+                )"
+            >
+                <Icon
+                    mode="svg"
+                    name="ph:list-checks-duotone"
+                />
+                <span>762</span>
+                <span>rules filtered</span>
+                <span class="opacity-50">out of 1718 rules</span>
             </div>
             <div class="w-1/3">
                 <InputGroup class="rounded-full">
                     <InputGroupInput placeholder="Search Rules..." />
                     <InputGroupAddon>
                         <Icon
-                            name="ph:magnifying-glass-duotone"
                             mode="svg"
+                            name="ph:magnifying-glass-duotone"
                         />
                     </InputGroupAddon>
                 </InputGroup>
